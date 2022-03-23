e3fd1a2f17947d29e6e1c99888689cc2b5418efc
Pablo Herranz <pherranz@stratio.com>
2022-03-23 12:08:09 +0100

    empiezo mapas

---
+++ CursoUdemy/FernandoHerrera/angular/09- mapasApp/src/app/mapas/mapas.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MapasRoutingModule } from './mapas-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MapasRoutingModule
+  ]
+})
+export class MapasModule { }
